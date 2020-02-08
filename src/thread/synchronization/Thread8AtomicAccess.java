4ed71baaa1442608d1e833a7b962b90072736f26
kouki <monem>
2020-02-08 21:40:06 +0100

    aa

--- src/thread/synchronization/Thread8AtomicAccess.java
+++ src/thread/synchronization/Thread8AtomicAccess.java
@@ -1,6 +1,8 @@
 package thread.synchronization;
 
-// http://blog.paumard.org/2011/05/06/synchronisation-et-volatilite/
+// see http://blog.paumard.org/2011/05/06/synchronisation-et-volatilite/
+// see http://tutorials.jenkov.com/java-concurrency/java-memory-model.html
+//see http://tutorials.jenkov.com/java-concurrency/volatile.html
 
 public  class Thread8AtomicAccess{
     public volatile int String;
